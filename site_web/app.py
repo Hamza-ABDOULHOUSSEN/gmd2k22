47ffc9bf7867f00d5e0949249249efb13912a07d
samib <sami.boulechfar@telecomnancy.eu>
2022-04-23 00:44:38 +0200

    Ajout version initiale de myPage

--- site_web/app.py
+++ site_web/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template
 from requests import *
 import forms
 
-
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
@@ -24,6 +23,23 @@ def ex_drugbank_query():
         count = len(content)
 
     return render_template("ex_drugbank_query.html", DrugbankFormQuery=DrugbankFormQuery, query=query, count=count, content=content)
+
+@app.route('/myPage', methods = ["GET"])
+def ex_drugbank_query2():
+    DrugbankFormQuery = forms.DrugbankFormQuery(request.form)
+    arguments = request.args
+
+    query = "none"
+    content = []
+    count = 0
+
+    if len(arguments) != 0:
+        query = arguments["query"]
+        content = drugbank_query(query)
+        count = len(content)
+
+    return render_template("myPage.html", DrugbankFormQuery=DrugbankFormQuery, query=query, count=count, content=content)
+
 
 if __name__ == '__main__':
     app.run()
