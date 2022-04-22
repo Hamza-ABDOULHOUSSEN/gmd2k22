329974e720f7ef2db5c3e3e71aa04350a73cb998
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2022-04-22 20:55:04 +0200

    Add test for new queries

--- python/test/test_drugbank_index.py
+++ python/test/test_drugbank_index.py
@@ -9,3 +9,10 @@ def test_search():
     assert(len(content) == 1)
     assert(content[0][0] == "DB00001")
     assert(content[0][1] == "lepirudin")
+
+    # test double * * in queries
+    ## grep - c "<drugbank-id primary=\"true\">.B000.." drugbank.xml
+    ## result : 93
+
+    content = drugbank_search('"id : *B000*"')
+    assert len(content) == 93
