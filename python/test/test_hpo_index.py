1baa8730af5b19e1e4901adb9918b1485ef8d8c3
ALLOUCHE Kamal <kamal.allouche@telecomnancy.eu>
2022-04-25 15:56:38 +0000

    Lier Stitch et Sider

--- python/test/test_hpo_index.py
+++ python/test/test_hpo_index.py
@@ -1,5 +1,6 @@
 import pytest
 from hpo.hpo_index_query import hpo_search
+import sys
 
 def test_search():
     #content = stitch_chemical_sources_search('"source_format : ATC"')
@@ -7,3 +8,12 @@ def test_search():
 
     content = hpo_search('"* : *"')
     assert(len(content) == 12299)
+
+    query = "\"hpo_id : HP_0000050\""
+
+    argc = len(sys.argv)
+    if (argc > 1):
+        query = sys.argv[1]
+        query = f'"{query}"'
+
+    assert(len(query) == 24)
