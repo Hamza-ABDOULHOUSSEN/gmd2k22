6f8b714463df2bea1972bc1856db4997d5b8c503
BOULECHFAR Sami <sami.boulechfar@telecomnancy.eu>
2022-04-25 16:48:05 +0200

    Deuxieme version du site

--- site_web/app.py
+++ site_web/app.py
@@ -25,7 +25,7 @@ def ex_drugbank_query():
     return render_template("ex_drugbank_query.html", DrugbankFormQuery=DrugbankFormQuery, query=query, count=count, content=content)
 
 @app.route('/myPage', methods = ["GET"])
-def ex_drugbank_query2():
+def myPage():
     DrugbankFormQuery = forms.DrugbankFormQuery(request.form)
     arguments = request.args
 
@@ -40,6 +40,36 @@ def ex_drugbank_query2():
 
     return render_template("myPage.html", DrugbankFormQuery=DrugbankFormQuery, query=query, count=count, content=content)
 
+@app.route('/requete', methods = ["GET"])
+def requete():
+    #query = "SELECT *"
+    query = "\"id : DB0000*\""
+    content = drugbank_query(query)
+    return render_template("requete.html", content=content)
+
+@app.route('/test', methods = ["GET"])
+def test():
+    DrugbankFormQuery = forms.DrugbankFormQuery(request.form)
+    arguments = request.args
+
+    query = "none"
+    content = []
+    count = 0
+
+    if len(arguments) != 0:
+        query = arguments["query"]
+        content = drugbank_query(query)
+        count = len(content)
+
+    return render_template("test.html", DrugbankFormQuery=DrugbankFormQuery, query=query, count=count, content=content)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
+    query = "\"id : DB0000*\""
+    content = drugbank_query(query)
+    print(str(content))
