0b30bad3a72ed97a31ff5ccae0fd7d01c6a63d4b
Kamal ALLOUCHE <kamal.allouche@telecomnancy.eu>
2022-04-25 17:41:44 +0200

    Revert "Lier Sider et Stitch"
    This reverts commit c2771cf2f48e036b5d9257ade5140cc1f37ace90.

--- python/link.py
+++ python/link.py
@@ -4,23 +4,21 @@ from omim.omim_index_query import omim_search
 from stitch.stitch_chemical_sources_index_query import stitch_chemical_sources_search
 from stitch.stitch_br08303_index_query import stitch_br08303_search
 
-def replace1m(text): # CID1... to CIDm...
-    text = str(text)
-    text = text[0:3] + "m" + text[4:]
+def replace1m(text):
+    text = ""
     return text
+    ## CID1... to CIDm...
 
-def replace0s(text): # CID0... to CIDs...
-    text = str(text)
-    text = text[0:3] + "s" + text[4:]
+def replace0s(text):
+    text = ""
     return text
+    ## CID0... to CIDs...
 
 def sider_to_stitch_compoundid1(id1, id2):
-    ida = replace1m(id1)
-    idb = replace0s(id2)
-    query = '"chemical : ' + ida + ' OR chemical : ' + idb + '"'
-    table = stitch_chemical_sources_search(query)
-    print(table)
+    table = []
+    return table
     # retourne la liste des source_code
+
 
 def stitch_source_code_to_stitch_atc_code(code):
     table = []
@@ -31,5 +29,3 @@ def stitch_atc_code_to_drugbank(atc_code):
     table = []
     return table
     # retourne les listes [[name, description, ...]]
-
-sider_to_stitch_compoundid1("CID100003269","CID000005756")
--- python/test/test_hpo_index.py
+++ python/test/test_hpo_index.py
@@ -1,6 +1,5 @@
 import pytest
 from hpo.hpo_index_query import hpo_search
-import sys
 
 def test_search():
     #content = stitch_chemical_sources_search('"source_format : ATC"')
@@ -8,12 +7,3 @@ def test_search():
 
     content = hpo_search('"* : *"')
     assert(len(content) == 12299)
-
-    query = "\"hpo_id : HP_0000050\""
-
-    argc = len(sys.argv)
-    if (argc > 1):
-        query = sys.argv[1]
-        query = f'"{query}"'
-
-    assert(len(query) == 24)
