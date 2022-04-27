85f1e5e907916daf1054475b3646a45534976d57
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2022-04-27 13:12:02 +0200

    Update to have partial results

--- site_web/requests.py
+++ site_web/requests.py
@@ -8,6 +8,19 @@ from drugbank.drugbank_index_query import *
 def drugbank_query(query):
     return drugbank_search(query)
 
-def sb(query):
-    disease_list, curing_drug_list, side_effects_from_drug_list = complete_search(query)
+def complete_search(symptom, content_sider_id):
+
+    # correction of the input
+    symptom = symptom.lower()
+
+    # initiation of global lists
+    disease_list = []
+    curing_drug_list = []
+    side_effects_from_drug_list = []
+
+    disease_list = search_disease_from_symptom(symptom, disease_list)
+    curing_drug_list = search_curing_drug_from_symtom(symptom, curing_drug_list)
+
+    side_effects_from_drug_list = search_side_effects_drug_from_content_sider_id(content_sider_id, side_effects_from_drug_list)
+
     return disease_list, curing_drug_list, side_effects_from_drug_list
